1533feb458cfd3f4c45a4e140731a9725aec3b6b
Frowingg <edbico0@gmail.com>
2022-06-09 23:12:11 +0200

    jsnack3 100%

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,12 +1,31 @@
-let sum = 0;
-let num;
-const gameResult = document.getElementById('game-result')
+// let sum = 0;
+// let num;
+// let userNumbers;
+// const gameResult = document.getElementById('game-result')
 
-for(let i = 0; i < 10; i++) {
-    num = parseInt(prompt('dammi un numero'));
-    sum += num;
-}
+// for(let i = 0; i < 10; i++) {
+//     userNumbers.getElementById('user-numbers').value
+//     userNumbers += []
+//     // num = parseInt(prompt('dammi un numero'));
+//     sum += num;
+// }
+// gameResult.innerHTML = sum
+
+let sum = 0;
+let userNumber = document.getElementById('user-number');
+const gameResult = document.getElementById('game-result');
+const sumBtn = document.getElementById('sum-btn')
 gameResult.innerHTML = sum
+console.log(typeof(sum))
+
+sumBtn.addEventListener('click',
+    function() {
+        console.log(typeof(parseInt(userNumber.value)))
+        sum += parseInt(userNumber.value)
+        console.log(sum)
+        gameResult.innerHTML = sum
+    }
+)
 
 
 
