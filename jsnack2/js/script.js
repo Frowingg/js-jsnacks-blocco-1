753d83e892f2c684f211fb8d3bb310e209a8ad83
Frowingg <edbico0@gmail.com>
2022-06-09 18:10:22 +0200

    jsnack2 100%

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,12 +1,24 @@
-const userNumbers= prompt('dammmi 4 cifre');
-
 const gameResult = document.getElementById('game-result');
-
+const sumBtn = document.getElementById('sum-btn');
+let userNumbers;
 let sum = 0;
 
-for (i=0; i<userNumbers.length; i++) {
 
-    let singleNumber = parseInt(userNumbers[i])
-    sum += singleNumber;
-    gameResult.innerHTML = sum
-}
+sumBtn.addEventListener('click',
+    function() {
+    userNumbers = document.getElementById('user-numbers').value;    
+    console.log(userNumbers) 
+    for(i=0; i<userNumbers.length; i++) {
+
+        let singleNumber = parseInt(userNumbers[i]);
+        console.log(singleNumber)
+
+        sum += singleNumber;
+        gameResult.innerHTML = sum;
+    }
+    }
+)
+
+
+
+
